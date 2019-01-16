e852864fb7b4384aeada0fbec7b249013905f84b
Mircea Catana <mircea.catalin.catana@gmail.com>
2019-01-16 08:35:23 +0200

    Added GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+static GLFWwindow* window;
+
 #include "vector.h"
 #include "framebuffer.h"
 #include "model.h"
 #include "rasterizer.h"
+
+bool initGL();
+void keyboard_input();
+void drawQuad();
 
 int main(int argc, char* argv[])
 {
@@ -13,6 +22,7 @@ int main(int argc, char* argv[])
     }
 
     Color red(255u, 120u, 0u);
+    Color green(20u, 255u, 120u);
     Color gray(50u, 50u, 50u);
 
     Model model(argv[1]);
@@ -44,13 +54,81 @@ int main(int argc, char* argv[])
         float x2 = (v2.x() * 0.7f + 1.0f) * halfWidth;
         float y2 = (v2.y() * 0.7f + 1.0f) * halfHeight;
 
-        drawLine(framebuffer.output(), Vector2(x0, y0), Vector2(x1, y1), red);
-        drawLine(framebuffer.output(), Vector2(x0, y0), Vector2(x2, y2), red);
-        drawLine(framebuffer.output(), Vector2(x1, y1), Vector2(x2, y2), red);
+        drawLine(framebuffer.output(), Vector2(x0, y0), Vector2(x1, y1), green);
+        drawLine(framebuffer.output(), Vector2(x0, y0), Vector2(x2, y2), green);
+        drawLine(framebuffer.output(), Vector2(x1, y1), Vector2(x2, y2), green);
     }
 
 
     framebuffer.output().writePPM("out.ppm");
 
+    /********* GL *********/
+
+    if (!initGL()) {
+        return 0;
+    }
+
+    glClearColor(0.12f, 0.11f, 0.14f, 0.0f);
+
+    while (glfwWindowShouldClose(window) == 0) {
+        keyboard_input();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawQuad();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
     return 0;
+}
+
+bool initGL()
+{
+    // Initialise GLFW
+    if(!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return false;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window
+    window = glfwCreateWindow(1024, 768, "W1S1", nullptr, nullptr);
+    if(window == nullptr) {
+        std::cerr <<  "Failed to open GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    return true;
+}
+
+void keyboard_input()
+{
+    glfwPollEvents();
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, 1);
+    }
+}
+
+void drawQuad() {
+
 }
